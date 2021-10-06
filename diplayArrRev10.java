9ab2731403e2e378f0f3fed5353d0c9e82d7db9f
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2021-10-06 22:47:36 +0530

    10

--- diplayArrRev10.java
+++ diplayArrRev10.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class main9 {
+class main10 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
@@ -8,16 +8,15 @@ class main9 {
         for (int i = 0; i < n; i++) {
             arr[i] = scn.nextInt();
         }
-        displayArray(arr, 0);
+        displayArrRev(arr, 0);
         scn.close();
     }
 
-    public static void displayArray(int[] arr, int n) {
+    public static void displayArrRev(int[] arr, int n) {
         if (n == arr.length) {
             return;
         }
-        System.out.print(arr[n]+ " ");
-        displayArray(arr, n + 1);
-
+        displayArrRev(arr, n + 1);
+        System.out.print(arr[n] + " ");
     }
 }
