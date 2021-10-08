c0278a64ac2207f72d2debd86bcedbe510f22c44
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2021-10-08 10:41:48 +0530

    14_1 buffered stream

--- allIndex14.java
+++ allIndex14.java
@@ -1,13 +1,13 @@
-import java.io.BufferedReader;
-import java.util.Scanner;
+import java.io.*;
 
 class main14 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
+        String[] input = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(input[i]);
         }
         int x = Integer.parseInt(br.readLine());
         int[] iarr = allIndex(arr, x, 0, 0);
